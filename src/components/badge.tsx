af27e2ecf05c09a9d920628f1c07459dc7c078bb
Prajna <prajnapprabhu3@gmail.com>
2024-02-26 10:03:45 +0530

    fix: react to lucide switch

--- src/components/badge.tsx
+++ src/components/badge.tsx
@@ -1,4 +1,4 @@
-import { FaGithub } from "react-icons/fa";
+import { Github } from "lucide-react";
 
 const Badge = () => {
   return (
@@ -7,7 +7,7 @@ const Badge = () => {
       target="_blank"
       className="flex items-center text-[10.5px] font-medium bg-white border rounded-full px-2 py-[2px]"
     >
-      <FaGithub className="text-[11px] mr-1" />
+      <Github size={12} className="text-[11px] mr-1" />
       <p>Star the project on github</p>
     </a>
   );
